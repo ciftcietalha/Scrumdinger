b6d84cb101d725ea49417bf288f17db99bc1db5b
Enes Ciftci <156692114+ciftcietalha@users.noreply.github.com>
2025-08-02 14:50:27 +0700

    Displaying Data in List

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -4,7 +4,8 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.sampleData)
+        
         }
     }
 }
