edb0105b387b9d27cc8cfb19967c8ab307c0c572
Enes Ciftci <156692114+ciftcietalha@users.noreply.github.com>
2025-08-17 22:23:41 +0700

    Added new edit feature

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,52 @@
+import SwiftUI
+
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    @State private var attendeeName = ""
+
+
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue(Text("\(scrum.lengthInMinutes) minutes"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                        .accessibility(hidden: true)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    scrum.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $attendeeName)
+                    Button(action:{
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: attendeeName)
+                            scrum.attendees.append(attendee)
+                            attendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel(Text("Add Attendee"))
+                    }
+                    .disabled(attendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    DetailEditView()
+}
