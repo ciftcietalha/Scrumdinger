c8e6ca09b056a6422bf8d5f700474a9eacbeb436
Enes Ciftci <156692114+ciftcietalha@users.noreply.github.com>
2025-11-22 20:27:54 +0700

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+
+@main
+struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.sampleData
+
+
+    var body: some Scene {
+        WindowGroup {
+            ScrumsView(scrums: $scrums)
+        }
+    }
+}
